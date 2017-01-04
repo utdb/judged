8c6f6256a65f8f3c052289fffe6e0933fa1fd0d7
Brend Wanders <b.wanders@utwente.nl>
2017-01-04 15:07:19 +0100

    Clean up test code

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,14 +2,8 @@
 
 from tests.lawful import run_tests
 
-from tests import test_core
-from tests import test_tokenizer
-from tests import test_parser
-from tests import test_prover
-from tests import test_cases
-
-from tests import test_worlds
-from tests import test_knowledge
+def main():
+    run_tests()
 
 if __name__ == '__main__':
-    run_tests()
+    main()
